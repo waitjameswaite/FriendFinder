d777b3cf737bda87835dbed2c6efad959b819ef6
James Waite <James Waite>
2017-10-17 16:03:04 -0400

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+{
+	"name": "James Waite",
+	"photo": "http://4.bp.blogspot.com/-nRZ_RF2797M/UbkMUVuxR3I/AAAAAAAAAIQ/RlThd-lXCHg/s1600/black_shepherd_german_shepherd_grass_sitting_dog_57806_3600x2400.jpg",
+	"scores": [
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1
+	]
+}
